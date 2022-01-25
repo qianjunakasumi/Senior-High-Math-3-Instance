548a12ff1d26ddb7c788f259a3d2fe2eef19dc60
千橘 雫霞 <i@qianjunakasumi.ren>
2022-01-25 13:18:43 +0800

    bye!🗑️: delete this project

--- 1.1 算法与程序框图/1.1.1 算法的概念/例1/main.go
+++ 1.1 算法与程序框图/1.1.1 算法的概念/例1/main.go
@@ -1,4 +1,10 @@
-// Copyright (c) 2020 Qianjunakasumi千橘雫霞 | MIT License
+// Copyright (c) 2020 qianjunakasumi <i@qianjunakasumi.ren>
+//                    qianjunakasumi <qianjunakasumi@outlook.com>
+//                    [qianjunakasumi] 千橘雫霞(https://github.com/qianjunakasumi)
+//
+//      This Source Code Form is subject to the terms of the Mozilla Public
+//      License, v. 2.0. If a copy of the MPL was not distributed with this
+//      file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 package main
 
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/qianjunakasumi/Math3
-
-go 1.15
